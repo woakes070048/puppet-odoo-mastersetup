0b7fe551ae93e59687090b8bdff58f6532ff94c4
Brandon Haymore <bhaymore@local-motors.com>
2015-06-24 15:56:58 -0700

    setup for different environments

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,15 @@
-include roles::puppetmaster
+node /^puppet/ {
+  include roles::puppetmaster
+}
+
+node /^prod/ {
+  include roles::production
+}
+
+node /^qa/ {
+  include roles::qa
+}
+
+node /^dbbackup/ {
+  include roles::dbbackup
+}
