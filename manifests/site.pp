134f723396d75f9e3566fff53af246da37f528d8
Brandon Haymore <bhaymore@local-motors.com>
2015-09-08 00:26:44 -0700

    updated to just included puppetmaster roles

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,3 @@
 node /^puppet/ {
   include roles::puppetmaster
 }
-
-node /^prod/ {
-  include roles::production
-}
-
-node /^qa/ {
-  include roles::qa
-}
-
-node /^dbbackup/ {
-  include roles::dbbackup
-}
