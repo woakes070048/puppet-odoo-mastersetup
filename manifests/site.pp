114e9410f80971f019e22f0abf515995df5b7967
Brandon Haymore <bhaymore@local-motors.com>
2015-09-08 00:30:01 -0700

    updated for all dev environments

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,13 @@
-node /^dev/ {
+node /^odoo-dev/ {
   include roles::dev
+}
+node /^odoo-qa/ {
+  include roles::qa
 }
 
 node /^vagrant-public/ {
   include roles::vagrant_public
 }
-
 node /^vagrant-private/ {
   include roles::vagrant_private
 }
